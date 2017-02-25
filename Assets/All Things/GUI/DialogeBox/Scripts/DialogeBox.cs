507280bc76faabdcbdaaab804eec784b91affecc
Dekuguy <julian.forster@hotmail.com>
2017-02-25 13:57:25 +0100

    Bugfixes in DialogeBox +
    Fixed some Bugs in the DialogeBox script:
    - In the last box the alst 2 characters werent showed.
    
    Added the DiaogeBox Template:
    - showed the max. Characters in one Box
    
    Made Visuals of the GUI for diffrent resolutions compatible (16:10 and
    16:9)

--- Assets/All Things/GUI/DialogeBox/Scripts/DialogeBox.cs
+++ Assets/All Things/GUI/DialogeBox/Scripts/DialogeBox.cs
@@ -5,155 +5,160 @@ using UnityEngine.UI;
 
 public class DialogeBox : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject Visuals;
-    [SerializeField]
-    private GameObject NextIcon;
-    [SerializeField]
-    private Text TextObject;
+	[SerializeField]
+	private GameObject Visuals;
+	[SerializeField]
+	private GameObject NextIcon;
+	[SerializeField]
+	private Text TextObject;
 
-    private string rawText;
+	private string rawText;
 
-    [SerializeField]
-    private string[] TextBlocks;
-    private int TextBlockCount = 0;
-    [SerializeField]
-    private string[] CurrentTextBlockWords;
-    private int CurrentTextBlockWordsCount = 0;
+	[SerializeField]
+	private string[] TextBlocks;
+	private int TextBlockCount = 0;
+	[SerializeField]
+	private string[] CurrentTextBlockWords;
+	private int CurrentTextBlockWordsCount = 0;
 
-    private string toDisplayText;
+	private string toDisplayText;
 
-    private bool isShowing;
-    private bool isTriggering;
+	private bool isShowing;
+	private bool isTriggering;
 
-    private bool isComplex;
+	private bool isComplex;
 
-    public static DialogeBox Instance;
-    public static void Show(string text, bool isComplex)
-    {
-        Instance.Save(text, isComplex);
-    }
+	public static DialogeBox Instance;
+	public static void Show(string text, bool isComplex)
+	{
+		Instance.Save(text, isComplex);
+	}
 	public static bool ShowBox()
 	{
 		return Instance.isShowing;
 	}
 
-    void Start()
-    {
-        Instance = this;
+	void Start()
+	{
+		Instance = this;
 
-        Visuals.SetActive(false);
-        TextObject.enabled = false;
-        NextIcon.SetActive(false);
-    }
+		Visuals.SetActive(false);
+		TextObject.enabled = false;
+		NextIcon.SetActive(false);
+	}
 
-    private void Save(string code, bool isComplex)
-    {
-        Visuals.SetActive(true);
-        TextObject.enabled = true;
+	private void Save(string code, bool isComplex)
+	{
+		Visuals.SetActive(true);
+		TextObject.enabled = true;
 
-        isShowing = true;
+		isShowing = true;
 
-        WorldFunktions.FreezeLivingMovement(true);
+		WorldFunktions.FreezeLivingMovement(true);
 
-        this.isComplex = isComplex;
-        rawText = code;
-        Decode();
-        ShowNextTextBlock();
-    }
+		this.isComplex = isComplex;
+		rawText = code;
+		Decode();
+		ShowNextTextBlock();
+	}
 
-    private void Decode()
-    {
-        if (isComplex)
-        {
-            TextBlocks = rawText.Split(new string[] { "<new>" }, System.StringSplitOptions.RemoveEmptyEntries);
-            for (int i = 1; i < TextBlocks.Length; i++)
-            {
-                TextBlocks[i] = TextBlocks[i].Substring(2);
-            }
-            for (int i = 0; i < TextBlocks.Length; i++)
-            {
-                TextBlocks[i] = TextBlocks[i].Substring(0, TextBlocks[i].Length - 2);
-            }
-        }
-        else
-        {
-            TextBlocks = new string[1] { rawText };
-        }
-    }
-    private void ShowNextTextBlock()
-    {
-        toDisplayText = "";
+	private void Decode()
+	{
+		if (isComplex)
+		{
+			TextBlocks = rawText.Split(new string[] { "<new>" }, System.StringSplitOptions.RemoveEmptyEntries);
+			for (int i = 1; i < TextBlocks.Length; i++)
+			{
+				TextBlocks[i] = TextBlocks[i].Substring(2);
+			}
+			for (int i = 0; i < TextBlocks.Length; i++)
+			{
+				if( i == TextBlocks.Length-1)
+					TextBlocks[i] = TextBlocks[i].Substring(0, TextBlocks[i].Length);
+				else
+					TextBlocks[i] = TextBlocks[i].Substring(0, TextBlocks[i].Length - 2);
+			}
+		}
+		else
+		{
+			TextBlocks = new string[1] { rawText };
+		}
+	}
+	private void ShowNextTextBlock()
+	{
+		toDisplayText = "";
 
-        if (TextBlockCount < TextBlocks.Length)
-        {
-            CurrentTextBlockWords = TextBlocks[TextBlockCount].Split(' ');
-            TextBlockCount++;
-            CurrentTextBlockWordsCount = 0;
-            isTriggering = true;
-        }
-        else
-        {
-            isTriggering = false;
-            End();
-        }
-    }
+		if (TextBlockCount < TextBlocks.Length)
+		{
+			CurrentTextBlockWords = TextBlocks[TextBlockCount].Split(' ');
 
-    private void GetTriggeredText()
-    {
-        if (CurrentTextBlockWordsCount < CurrentTextBlockWords.Length)
-        {
-            if (CurrentTextBlockWordsCount == 0)
-                toDisplayText += CurrentTextBlockWords[CurrentTextBlockWordsCount];
-            else
-                toDisplayText += " " + CurrentTextBlockWords[CurrentTextBlockWordsCount];
-            CurrentTextBlockWordsCount++;
-        }
-        else
-        {
-            isTriggering = false;
-            NextIcon.SetActive(true);
-        }
-    }
+			TextBlockCount++;
+			CurrentTextBlockWordsCount = 0;
 
-    private void End()
-    {
-        WorldFunktions.FreezeLivingMovement(false);
+			isTriggering = true;
+		}
+		else
+		{
+			isTriggering = false;
+			End();
+		}
+	}
 
-        isShowing = false;
+	private void GetTriggeredText()
+	{
+		if (CurrentTextBlockWordsCount < CurrentTextBlockWords.Length)
+		{
+			if (CurrentTextBlockWordsCount == 0)
+				toDisplayText += CurrentTextBlockWords[CurrentTextBlockWordsCount];
+			else
+				toDisplayText += " " + CurrentTextBlockWords[CurrentTextBlockWordsCount];
+			CurrentTextBlockWordsCount++;
+		}
+		else
+		{
+			isTriggering = false;
+			NextIcon.SetActive(true);
+		}
+	}
 
-        Visuals.SetActive(false);
-        TextObject.enabled = false;
-        NextIcon.SetActive(false);
+	private void End()
+	{
+		WorldFunktions.FreezeLivingMovement(false);
 
-        TextBlockCount = 0;
-        toDisplayText = "";
-        CurrentTextBlockWordsCount = 0;
-    }
+		isShowing = false;
 
-    void Update()
-    {
-        if (isShowing)
-        {
-            if (isTriggering)
-            {
-                GetTriggeredText();
-                TextObject.text = toDisplayText;
-            }
+		Visuals.SetActive(false);
+		TextObject.enabled = false;
+		NextIcon.SetActive(false);
 
-            if (Input.GetButtonDown("Interact"))
-            {
-                if (isTriggering)
-                {
-                    isTriggering = false;
-                    TextObject.text = string.Join(" ", CurrentTextBlockWords);
-                }
-                else
-                {
-                    NextIcon.SetActive(false);
-                    ShowNextTextBlock();
-                }
-            }
-        }
-    }
+		TextBlockCount = 0;
+		toDisplayText = "";
+		CurrentTextBlockWordsCount = 0;
+	}
+
+	void Update()
+	{
+		if (isShowing)
+		{
+			if (isTriggering)
+			{
+				GetTriggeredText();
+				TextObject.text = toDisplayText;
+			}
+
+			if (Input.GetButtonDown("Interact"))
+			{
+				if (isTriggering)
+				{
+					isTriggering = false;
+					TextObject.text = string.Join(" ", CurrentTextBlockWords);
+				}
+				else
+				{
+					NextIcon.SetActive(false);
+					ShowNextTextBlock();
+				}
+			}
+		}
+	}
 }
